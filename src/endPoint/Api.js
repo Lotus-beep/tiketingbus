a3002942964e2f5008cefe5f1a5325c18f88b8db
Fadilah <kurniafadilah179@gmail.com>
2025-07-11 07:50:17 -0700

    perubahanv3

--- src/endPoint/Api.js
+++ src/endPoint/Api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-const BASE_URL = "http://127.0.0.1:8000/api/";
+const BASE_URL = "https://127.0.0.1:8000/api/";
 const LOGIN_URL = `${BASE_URL}token/`;
 const GET_NOTES = `${BASE_URL}notes/`;
 const GETDATA_URL = `${BASE_URL}getdata/`
